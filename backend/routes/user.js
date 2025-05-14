9e7b7a032e81cc3544ab99dbd151589b1faf6b76
yaswanthreddy <gujjalayaswanth@gmail.com>
2025-05-14 10:15:45 +0530

    done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,17 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-
 router.post("/", async (req, res) => {
   const { username, email, password } = req.body;
   try {
@@ -21,6 +10,15 @@ router.post("/", async (req, res) => {
     res.status(201).json(user);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
