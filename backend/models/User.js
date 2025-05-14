aeaf429f9b78ca739e504ee95928e7231d40d2f4
yaswanthreddy <gujjalayaswanth@gmail.com>
2025-05-14 14:52:17 +0530

    done

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
   role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
